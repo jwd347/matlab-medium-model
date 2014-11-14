d19e9997bfe4f442b1fa28c21528a1904a17141c
ceres.power@gmail.com <ceres.power@gmail.com@4510da55-cd1a-c183-de84-2be06906c448>
2014-11-14 16:21:53 +0000

    MediumModel - extensions to work with vectors.

--- trunk/@MediumModel/findTFromH.m
+++ trunk/@MediumModel/findTFromH.m
@@ -1,16 +1,28 @@
 function tOut = findTFromH(me,h,swtPlot)
-% Function to find T given h for a mixture of gases.
+% Function to find T given h for a mixture of gases of compositions me.Z.
 % Use fminsearch to find the T given the function for h.
+% me.T, me.Z and h should be the same row size.
 
-tOutGuess = me.T;
-hnd= @(T) abs(h - getH(me,T));
-[tOut,FVAL,EXITFLAG,OUTPUT] = fminsearch(hnd,tOutGuess);
+if size(me.Z,1)==1
+    Z=repmat(me.Z,size(me.T,1),1);
+else
+    Z=me.Z;
+end
 
-if nargin>2 & swtPlot
-    T_V = [0:10:1500]+273.15;
-    for ctT=1:length(T_V)
-        h_V(ctT) = getH(me,T_V(ctT));
-    end
+ctNoNan = find(~isnan(me.T));
+tOut = me.T;
+tOut(~ctNoNan) = 0;
+
+for ctT=ctNoNan'
+    tOutGuess = me.T(ctT);
+    hnd= @(T) abs(h(ctT) - getH(me,T,Z(ctT,:)));
+    [tOut(ctT),FVAL,EXITFLAG,OUTPUT] = fminsearch(hnd,tOutGuess);
+end
+
+% Optional plot - only for scalar case
+if nargin>2 & swtPlot & length(h)==1
+    T_V = (0:10:1500)+273.15;
+    h_V = getH(me,T_V,Z);
     figure;
     plot(h_V,T_V);grid on;hold on;
     plot(h,tOut,'r+','MarkerSize',12);
@@ -19,37 +31,25 @@ if nargin>2 & swtPlot
 end
 end
 
-function h = getH(me,T)
+function h = getH(me,T,Z)
 % Function to evaluate only h - taken from props function
 
 % Ho(T)/RT = –a1T –2 + a2lnT/T + a3 + a4T/2 + a5T 2/3 + a6T 3/4 + a7T 4/5 + b1/T (2)
 X_h0_M =  [-T.^-2 log(T)./T ones(size(T)) T./2 (T.^2)./3 (T.^3)./4 (T.^4)./5 T.^-1];
 h_V=ones(length(T),length(me.names));
-me.mm_V=zeros(1,length(me.names));
 for ctGas =1:length(me.names)
     GasName = me.names{ctGas};
-    me.mm_V(ctGas) = me.gas.(GasName).mm;
     ctTRanges = length(me.gas.(GasName).tRange);
     for ctRange = 1:ctTRanges
         tRange=me.gas.(GasName).tRange{ctRange};
-        ind= find((tRange(1)<=T)&(T<=tRange(2))); %#ok<PROP>
+        ind= find((tRange(1)<=T)&(T<=tRange(2))); 
         if isempty(ind)
             continue;
         end
         h_V(ind,ctGas) = (X_h0_M(ind,:)*[me.gas.(GasName).a{ctRange} me.gas.(GasName).b{ctRange}(1)]').*T(ind);
     end
 end
-h_V=h_V.*me.R; %#ok<PROP>
-
-if isempty(me.Zeq)
-    Z=me.Z;
-else
-    Z=me.Zeq;
-end
-
-if any(abs(sum(Z,2)-1)>0.0001)
-    warning('MediumModel:props:Composition vector does not sum to 1(min = %f,max= %f',min(sum(Z,2)),max(sum(Z,2)))
-end
+h_V=h_V.*me.R; 
 h = sum(h_V.*Z,2);
 end
 
--- trunk/@MediumModel/findTFromHEq.m
+++ trunk/@MediumModel/findTFromHEq.m
@@ -1,20 +1,35 @@
 function tOut = findTFromHEq(me,h,swtPlot)
-% Function to find T given h for a mixture of gases after equilibrating 
+% Function to find T given h for a mixture of gases of compositions me.Z after equilibrating 
 % according to the reaction defined in me.nu.
 % Use fminsearch to find the T given the function for h using solveEq at each step.
 % NB this function takes much longer than findTFromH.
+% me.T, me.Z and h should be the same row size.
 
 if isempty(me.nu), error('MediumModel:findTFromHEq_NoNu','Error - no Medium nu set.'); end;
+T=me.T;
+if size(me.Z,1)==1
+    Z=repmat(me.Z,size(me.T,1),1);
+else
+    Z=me.Z;
+end
 
-tOutGuess = me.T;
-hnd= @(T) abs(h - getHEq(me,T));
-[tOut,FVAL,EXITFLAG,OUTPUT] = fminsearch(hnd,tOutGuess);
+ctNoNan = find(~isnan(me.T));
+tOut = T;
+tOut(~ctNoNan) = 0;
 
-if nargin>2 & swtPlot
-    T_V = [0:10:1500]+273.15;
-    for ctT=1:length(T_V)
-        h_V(ctT) = getHEq(me,T_V(ctT));
-    end
+for ctT=ctNoNan'
+    tOutGuess = T(ctT);
+    hnd= @(T) abs(h(ctT) - getHEq(me,T,Z(ctT,:)));
+    [tOut(ctT),FVAL,EXITFLAG,OUTPUT] = fminsearch(hnd,tOutGuess);
+end
+
+% Put back original T & Z
+me.setTandZ(T,Z);
+    
+% Optional plot - only for scalar case
+if nargin>2 & swtPlot & length(h)==1
+    T_V = (0:10:1500)+273.15;
+    h_V = getHEq(me,T_V,Z);
     figure;
     plot(h_V,T_V);grid on;hold on;
     plot(h,tOut,'r+','MarkerSize',12);
@@ -23,35 +38,28 @@ if nargin>2 & swtPlot
 end
 end
 
-function h = getHEq(me,T)
+function h = getHEq(me,T,Z)
 % Function to evaluate only h after equilibrating - taken from props function
 
 % Ho(T)/RT = –a1T –2 + a2lnT/T + a3 + a4T/2 + a5T 2/3 + a6T 3/4 + a7T 4/5 + b1/T (2)
 X_h0_M =  [-T.^-2 log(T)./T ones(size(T)) T./2 (T.^2)./3 (T.^3)./4 (T.^4)./5 T.^-1];
 h_V=ones(length(T),length(me.names));
-me.mm_V=zeros(1,length(me.names));
 for ctGas =1:length(me.names)
     GasName = me.names{ctGas};
-    me.mm_V(ctGas) = me.gas.(GasName).mm;
     ctTRanges = length(me.gas.(GasName).tRange);
     for ctRange = 1:ctTRanges
         tRange=me.gas.(GasName).tRange{ctRange};
-        ind= find((tRange(1)<=T)&(T<=tRange(2))); %#ok<PROP>
+        ind= find((tRange(1)<=T)&(T<=tRange(2))); 
         if isempty(ind)
             continue;
         end
         h_V(ind,ctGas) = (X_h0_M(ind,:)*[me.gas.(GasName).a{ctRange} me.gas.(GasName).b{ctRange}(1)]').*T(ind);
     end
 end
-h_V=h_V.*me.R; %#ok<PROP>
+h_V=h_V.*me.R; 
 
-me.setT(T);
+me.setTandZ(T,Z);
 me.solveEq;
-Z=me.Zeq;
-
-if any(abs(sum(Z,2)-1)>0.0001)
-    warning('MediumModel:props:Composition vector does not sum to 1(min = %f,max= %f',min(sum(Z,2)),max(sum(Z,2)))
-end
-h = sum(h_V.*Z,2);
+h = sum(h_V.*me.Zeq,2);
 end
 
