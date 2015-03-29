b185ffa463c33734830ddb2f84c26589bee24cfb
ceres.power@gmail.com <ceres.power@gmail.com@4510da55-cd1a-c183-de84-2be06906c448>
2015-03-29 21:20:02 +0000

    MediumModel update to the findTFromHEq method to make it faster. The temperature limits input is replaced by an h tolerance.

--- trunk/@MediumModel/SelfTestFindTFromH.m
+++ trunk/@MediumModel/SelfTestFindTFromH.m
@@ -10,17 +10,22 @@ disp('Running SelfTestFindTFromH - findTFromH.');
 oMed = MediumModel({'O2','N2'});
 
 % oMed.setTandZ([400,500]',[0.5 0.5;0.2 0.8]);
-oMed.setTandZ(400,[0.5 0.5]);
-oMed.T
-oMed.Z
-T = oMed.findTFromH(3500,1)
+T1 = 400;
+oMed.setTandZ(T1,[0.5 0.5]);
+h1 = oMed.h;
+T2 = oMed.findTFromH(h1,1);
+assert(abs(T1-T2)<0.001,['findTFromH failed at ' num2str(oMed.T) 'K, ' mat2str(oMed.Z)]);
+
 %%
 tic;
-for k=1:100
-    T = oMed.findTFromH([3500,4000]');
+ctCalc = 100;
+h1 = [3500:100:4000]';
+for k=1:ctCalc
+    T = oMed.findTFromH(h1);
 end
-toc
+tDuration = toc;
 Tans = T
+disp(['Duration for ' num2str(ctCalc) ' findTFromH vector calcs: ' num2str(tDuration)]);
 
 %%
 % Vector tests
@@ -42,19 +47,32 @@ T = oMed.findTFromH([3500,4000]')
 oMed.setTandZ([400,500]',[0.5 0.5;0.2 0.8]);
 T = oMed.findTFromH([3500,4000]')
 
+disp('Vector tests complete.');
+
 %% 
 % Test with liquid water
 oMed = MediumModel({'CH4','H2O','H2ObLb'});
+oMed.setTandZ(283.15,[0.5 0.25 0.25]);
+oMed.findTFromH(oMed.h,1)
+oMed.setT(599);
+oMed.findTFromH(oMed.h,1)
 
-Tin = [-10 -1 0 1 10 50 99 100 101 110]'+273.15;
-oMed.setTandZ(Tin,[0.5 0.25 0.25]);
+Tin = [0 1 10 50 99 100 101 110]'+273.15;
+oMed.setT(Tin);
 h = oMed.h;
 TReturn = oMed.findTFromH(h);
-TReturn - Tin
+TError = TReturn - Tin
+assert(max(abs(TReturn-Tin))<0.001,'findTFromH failed to return all matching temperatures.');
+
 % A warning is expected from cases below 273.15K.
 % A difference is expected here for cases below 273.15K.
 % To use this function with gases including H2ObLb below 273.15, separate
 % the problem into cases above & below 273.15.
+disp('Warnings expected...');
+Tin = [-10 -1]'+273.15;
+oMed.setT(Tin);
+h = oMed.h;
+TReturn = oMed.findTFromH(h);
 
 %% findTFromHEq Tests
 %  ==================
@@ -71,13 +89,32 @@ oMed.setTandZ(400,Z);
 oMed.setNu(nu);
 oMed.T
 oMed.Z
-T = oMed.findTFromHEq(-120000,50,[],1)
+[T,ctIter] = oMed.findTFromHEq(-120000,50,[],1)
 
 %%
 tic;
-for k=1:2
-    T = oMed.findTFromHEq(-100000);
-end
+lstGasFuel={'N2','O2','H2O','H2ObLb'};
+nu=[0 0 -1 1];
+Z=[0.75 0.15 0.1 0]; Z=Z/sum(Z);
+oMed = MediumModel(lstGasFuel);
+oMed.setTandZ(400,Z);
+oMed.setNu(nu);
+oMed.T
+oMed.Z
+[T,ctIter] = oMed.findTFromHEq(-22200,50,[],1)
+toc
+
+%%
+lstGasFuel={'H2','CH4','CO','CO2','H2O'};
+nu=[[3 -1  1  0 -1]; ...
+    [1  0 -1  1 -1]];
+Z=[0 1 0 0 2.8]; Z=Z/sum(Z);
+oMed = MediumModel(lstGasFuel);
+% oMed.setTandZ([400,500]',[0.5 0.5;0.2 0.8]);
+oMed.setTandZ(400,Z);
+oMed.setNu(nu);
+tic;
+T = oMed.findTFromHEq(-100000,0,0.1);
 toc
 Tans = T
 %%
@@ -93,16 +130,25 @@ T = oMed.findTFromHEq(h)
 % Test with liquid water
 oMed = MediumModel({'CH4','H2O','H2ObLb'});
 
-Tin = [-10 -1 0 1 10 50 99 100 101 110]'+273.15;
+Tin = [0 1 10 50 99 100 101 110]'+273.15;
 oMed.setTandZ(Tin,[0.5 0.25 0.25]);
 oMed.setNu([0 -1 1]);
 oMed.solveEq;
 oMed.setTandZ(Tin,oMed.Zeq);
 h = oMed.h;
-TReturn = oMed.findTFromHEq(h);
-TReturn - Tin
+tic;
+[TReturn,ctIter] = oMed.findTFromHEq(h,0,0.001);
+toc
+ctIter
+TError = TReturn - Tin
+assert(max(abs(TReturn-Tin))<0.0001,'findTFromH failed to return all matching temperatures.');
 
-% Test limiting of T
-oMed.setTandZ(500,[0.5 0.25 0.25]);
-T2 = oMed.findTFromHEq(h(1),0,[274,inf],1);
+%% Test beyond range of gas data
+oMed.setTandZ(-10+273.15,[0.5 0 0.5]);
+oMed.setNu([0 -1 1]);
+h1 = -1.85e5;
+disp('Warning expected...');
+TReturn1 = oMed.findTFromHEq(h1,0,1,1)
+h2 = -0.82e5;
+TReturn2 = oMed.findTFromHEq(h2,0,1,1)
 
--- trunk/@MediumModel/findTFromHEq.m
+++ trunk/@MediumModel/findTFromHEq.m
@@ -1,10 +1,11 @@
-function tOut = findTFromHEq(me,h,TAte,TLim,swtPlot)
-% tOut = findTFromHEq(me,h,TAte,TLim,swtPlot)
+function [tOut,ctIter] = findTFromHEq(me,h,tAte,hTol,swtPlot)
+% tOut = findTFromHEq(me,h,tAte,hTol,swtPlot)
 % Function to find T given h for a mixture of gases of initial compositions me.Z after  
-% equilibrating at the medium temperature-TAte according to the reaction defined in me.nu. 
-% TLim (optional) limits the search between 2 values of T - defaults to [-inf inf]. 
-% Uses fminsearch to find the T given the value of specific enthalpy h using solveEq at each step.
-% NB this function takes much longer than findTFromH.
+% equilibrating at the medium temperature-tAte according to the reaction defined in me.nu. 
+% tAte is an approach to equilibrium temperature.
+% hTol (optional, default 1J/mol) is a tolerance for the search. 
+% Uses an interpolation search to find the T given the value of specific enthalpy h using solveEq at each step.
+% NB this function takes much longer than findTFromDH.
 % me.T, me.Z and h should be the same row size. swtPlot only works for the scalar case.
 
 if isempty(me.nu), error('MediumModel:findTFromHEq_NoNu','Error - no Medium nu set.'); end;
@@ -13,17 +14,19 @@ if isempty(me.nu), error('MediumModel:findTFromHEq_NoNu','Error - no Medium nu s
 strFminOpt = optimset(@fminsearch);
 strFminOpt.TolFun=inf ;
 
-% Set default TAte
-if nargin<3 || isempty(TAte)
-    TAte = 0;
+% Set default tolerance
+if nargin<4 || isempty(hTol)
+    hTol = 1;
 end
 
-% Set default T range
-if nargin<4 || isempty(TLim)
-    TLim = [-inf inf];
+% Set default tAte
+if nargin<3 || isempty(tAte)
+    tAte = 0;
 end
 
+% Save starting values
 T=me.T;
+Zorig=me.Z;
 if size(me.Z,1)==1
     Z=repmat(me.Z,size(me.T,1),1);
 else
@@ -34,37 +37,130 @@ ctNoNan = find(~isnan(me.T));
 tOut = T;
 tOut(~ctNoNan) = 0;
 
+swtWarnBeyondRange = 0;
+ctIterMax = 50;
+ctIter = zeros(size(tOut));
+% Loop for all rows
 for ctT=ctNoNan'
-    tOutGuess = T(ctT);
-    hnd= @(T) abs(h(ctT) - getHEq(me,T,Z(ctT,:),strFminOpt,TLim,TAte));
-    [tOut(ctT),FVAL,EXITFLAG,OUTPUT] = fminsearch(hnd,tOutGuess);
+    % Find temperature range of all gases data
+    tMin = NaN; tMax = NaN;
+    tRanges = [-inf;inf]*ones(1,length(me.names));
+    for ctGas=1:length(me.names)
+        if Z(ctT,ctGas)<eps && all(me.nu(:,ctGas)==0)
+            continue
+        end
+        tRange = cell2mat(me.gas.(me.names{ctGas}).tRange)';
+        tRanges(:,ctGas) = [min(tRange);max(tRange)];
+        tMin = max(tMin,min(tRanges(:,ctGas))); % inner, narrowest gas ranges
+        tMax = min(tMax,max(tRanges(:,ctGas)));
+    end
+
+    % Find h at min T
+    me.setTandZ(tMin,Z(ctT,:));
+    me.solveEq([],strFminOpt,tAte);
+    h_V = me.h;
+    t_V = tMin;
+    if h(ctT)<=me.h && any(me.Zeq(tRanges(1,:)==tMin)>0.01)
+        % Extrapolate beyond low end
+        me.setTandZ(tMin+1,Z(ctT,:));
+        me.solveEq([],strFminOpt,tAte);
+        tOut(ctT) = interp1([h_V me.h],[tMin tMin+1],h(ctT),'linear','extrap');
+        swtWarnBeyondRange = 1;
+        continue
+    end
+    
+%     hndF=figure;plot(h_V,t_V,'*');hold on; % debug plot
+
+    % Find h at max T
+    me.setTandZ(tMax,Z(ctT,:));
+    me.solveEq([],strFminOpt,tAte);
+    hNew = me.h;
+    tNew = tMax;
+    if h(ctT)>=me.h && any(me.Zeq(tRanges(2,:)==tMax)>0.01)
+        % Extrapolate beyond high end
+        me.setTandZ(tMax-1,Z(ctT,:));
+        me.solveEq([],strFminOpt,tAte);
+        tOut(ctT) = interp1([me.h hNew],[tMax-1 tMax],h(ctT),'linear','extrap');
+        swtWarnBeyondRange = 1;
+        continue
+    end
+    
+    % If between limits, use pchip, else use linear which can extrapolate
+    if h(ctT)<hNew && h(ctT)>h_V
+        txtMethod = 'pchip';
+    else
+        txtMethod = 'linear';
+    end
+    
+    % Search for tOut(ctT) at h(ctT)
+    while abs(h(ctT)-hNew)>hTol
+        
+        [h_V,ind] = sort([h_V;hNew]);
+%         figure(hndF);plot(hNew,tNew,'*'); % debug plot
+        t_V = [t_V;tNew];
+        t_V = t_V(ind);
+        [h_U,indU] = unique(h_V);
+        t_U = t_V(indU);
+        tNew = interp1(h_U,t_U,h(ctT),txtMethod,'extrap');
+        
+        ctIter(ctT) = ctIter(ctT)+1;
+        
+        hNew = getHEq(me,tNew,Z(ctT,:),strFminOpt,tAte);
+        
+        if  ctIter(ctT)==ceil(ctIterMax/2)
+            % Getting nowhere - maybe inaccurate points exist so clear out a bit
+            % Replace all points within 0.1% of latest tNew with re-evaluated point at tNew
+            warning('MediumModel:findTFromHEq_IterMid',['Iteration reached clear-out at ' int2str(ctIter(ctT)) ' iterations.']);
+%             figure;plot(dH_V,t_V,'*'); hold on;% debug plot
+%             title(['findTFromHEq_IterMid searching at h=' num2str(h(ctT))]);
+%             vline(h(ctT),'r:');
+%             plot(hNew,tNew,'ro');
+            threshTV = (max(t_V)-min(t_V))/1000;
+            indKeep = find(t_V<tNew-threshTV | t_V>tNew+threshTV);
+            t_V = t_V(indKeep);
+            h_V = h_V(indKeep);
+        elseif  ctIter(ctT)>ctIterMax
+            warning('MediumModel:findTFromHEq_IterMax','Iteration reached maximum.');
+%             figure;plot(h_V,t_V,'*'); hold on;% debug plot
+%             title(['findTFromHEq_IterMax searching at h=' num2str(h(ctT))]);
+%             vline(h(ctT),'r:');
+%             plot(hNew,tNew,'ro');
+            break;
+        end
+        
+    end
+
+    tOut(ctT) = tNew;
+end
+
+if swtWarnBeyondRange
+    warning('MediumModel:findTFromHEq_OutOfRange','Beyond range of defined polynomial at least one gas - extrapolating.');
 end
     
 % Optional plot - only for scalar case
 if nargin>4 && swtPlot && length(h)==1
-    T_V = (0:10:2000)'+273.15;
-    h_V = getHEq(me,T_V,Z,strFminOpt,[-inf inf],TAte);
+    % Add detail close to the result
+    T_V = sort([linspace(tMin,tMax,50),...
+        linspace(max(tOut-100,tMin),min(tOut+100,tMax),25),...
+        linspace(max(tOut-10,tMin),min(tOut+10,tMax),25)]);
+    h_V = getHEq(me,T_V,Z,strFminOpt,tAte);
     figure;
-    plot(h_V,T_V);grid on;hold on;
+    plot(h_V,T_V,'-','MarkerSize',8);grid on;hold on;
     plot(h,tOut,'r+','MarkerSize',12);
     xlabel('h [J/mol]');
     ylabel('T [K]');
 end
 
 % Put back original T & Z
-me.setTandZ(T,Z);
+me.setTandZ(T,Zorig);
 
 end
 
-function h = getHEq(me,T,Z,strFminOpt,TLim,TAte)
+function h = getHEq(me,T,Z,strFminOpt,tAte)
 % Function to evaluate h after equilibrating
 
-if T(1)<TLim(1) || T(1)>TLim(2)
-    h = inf;
-else
-    me.setTandZ(T,Z);
-    me.solveEq([],strFminOpt,TAte);
-    h = me.h;
-end
+me.setTandZ(T,Z);
+me.solveEq([],strFminOpt,tAte);
+h = me.h;
 
 end
